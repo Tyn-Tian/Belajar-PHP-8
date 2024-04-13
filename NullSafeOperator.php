4e7fa51d8e13411b251373fd77e0bf841f79179d
lukxnut <kristian2015ok@gmail.com>
2024-04-13 15:28:55 +0700

    Add: Null Safe Operator

---
+++ NullSafeOperator.php
@@ -0,0 +1,25 @@
+<?php
+
+class Address 
+{
+    public ?string $country;
+}
+
+class User 
+{
+    public ?Address $address;
+}
+
+function getCountry(?User $user): ?string
+{
+    // Check secara manual
+    // if ($user != null) {
+    //     if ($user->address != null) {
+    //         return $user->address->country;
+    //     }
+    // }
+    //return null;
+    return $user?->address?->country;
+}
+
+echo getCountry(null) . PHP_EOL;
