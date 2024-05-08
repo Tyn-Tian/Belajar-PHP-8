a0f9a1d18bfc8ad1cd0133a8fb5f9406989be10b
Christian <kristian2015ok@gmail.com>
2024-05-08 14:36:02 +0700

    Add: Readonly Properties

---
+++ 8.1/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+class Category
+{
+    public function __construct(
+        public readonly string $id,
+        public readonly string $name
+    ) {
+    }
+}
+
+$category = new Category("1", "Gadget");
+var_dump($category->id);
+
+// $category->id = "2" // Error
